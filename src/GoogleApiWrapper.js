01745fd0a23b443b4dc266b80cf314c5d201ab68
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-02-01 05:09:44 +0200

    trying to get modals to work

--- src/GoogleApiWrapper.js
+++ src/GoogleApiWrapper.js
@@ -1,58 +1,115 @@
 import React, { Component } from 'react'
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react'
+import { Item, Grid, Icon, Image, Header, Modal, Button } from 'semantic-ui-react'
 
 const mapStyles = {
-    width: '50%',
-    height: '50%'
+  width: '100%',
+  height: '100%'
 }
 
 export class MapContainer extends Component {
-    state = {
-        windowInfo: false,
-        activeMarker: {},
-        selectedPlace: {}
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {},         //Shows the infoWindow to the selected place upon a marker
+    modalOpen: false,
+    markers: [{
+      title: "Tässä on nuppi",
+      name: "The marker`s title will appear as a tooltip.",
+      position: { lat: 37.778519, lng: -122.40564 }
+    }]
+  }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+      modalOpen: true
+    })
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
     }
+  }
 
-    onMarkerClick = (props, marker, e) =>
-        this.setState({
-            selectedPlace: props,
-            activeMarker: marker,
-            showingInfoWindow: true
-        })
+  onMapClicked = (t, map, coord) => {
+    const { latLng } = coord
+    const lat = latLng.lat()
+    const lng = latLng.lng()
+    this.setState({
+      markers: this.state.markers.concat({
+        title: 'Tässä on nuppi!',
+        name: 'Tähän tulee vaikka minkälaista tekstiä',
+        position: { lat: lat, lng: lng }
+      })
+    })
+  }
 
-    onClose = props => {
-        if (this.state.showingInfoWindow) {
-            this.setState({
-                showingInfoWindow: false,
-                activeMarker: null
-            })
-        }
-    }
+  renderMarkers = () => {
+    const markers = this.state.markers
+    return markers.map(marker => <Marker
+      name={marker.name}
+      position={marker.position}
+      onClick={this.onMarkerClick} />)
+  }
 
-    render() {
-        return (
-            <Map google={this.props.google}
-                zoom={2}
-                style={mapStyles}
-                initialCenter={{ lat: 51.4778, lng: -0.0014 }}>
-                <Marker
-                    onClick={this.onMarkerClick}
-                    name={'Kenyatta International Convention Centre'}
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+
+  render() {
+    return (<div>
+      <Modal
+        basic size='fullscreen'
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        centered={false}
+        closeIcon
+      >    <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Content image>
+          <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+          <Modal.Description>
+            <Header>Default Profile Image</Header>
+            <p>We've found the following gravatar image associated with your e-mail address.</p>
+            <p>Is it okay to use this photo?</p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+      <Map google={this.props.google}
+        zoom={2}
+        style={mapStyles}
+        initialCenter={{ lat: 0, lng: 0 }}
+        onClick={this.onMapClicked}>
+        {/*<InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+          size='big'>
+          <Item relaxed>
+            <div>{this.state.activeMarker === null ?
+              <h4>...</h4>
+              :
+              <div><h2>{this.state.activeMarker.title}</h2>
+                <h4>{this.state.activeMarker.name}</h4></div>
+            }<Button icon>
+                <Icon name='expand'
                 />
-                <InfoWindow marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}
-                    onClose={this.onClose}>
-                    <div>
-                        <h4>{this.state.selectedPlace.name}</h4>
-                    </div>
-                </InfoWindow>
-            </Map>)
-    }
+              </Button></div>
+          </Item>
+          </InfoWindow>*/}
+        {this.renderMarkers()}
+      </Map ></div>)
+  }
 }
 
 export default GoogleApiWrapper(({ apiKey, language }) => ({
-    apiKey: apiKey,
-    language: language
+  apiKey: apiKey,
+  language: language
 }))(MapContainer)
 
 
---
+++ src/LoginForm.js
@@ -0,0 +1,172 @@
+import React from 'react'
+import { Button, Form, Grid, Icon, Header, Image, Message, Segment } from 'semantic-ui-react'
+
+const LoginForm = () => (
+  <div className='login-form'>
+    <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='blue' textAlign='center'>
+          <Icon name='plane' /> Log in to account
+        </Header>
+        <Form size='large' /*loading success error field error*/>
+          <Segment>
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+
+            <Button color='blue' fluid size='large'>
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          <a href='#'>Create Account</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  </div>
+)
+
+export default LoginForm
+
+/*import React from 'react'
+import Blog from './components/Blog'
+import Togglable from './components/Togglable'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
+import loginService from './services/login'
+import blogService from './services/blogs'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      blogs: [],
+      user: null,
+      loginVisible: false,
+      message: '',
+      color: ''
+    }
+  }
+
+    const data = await blogService.getAll()
+    const blogs = data
+      .sort(compare)
+      .map(blog =>
+        <Blog
+          key={blog.id}
+          id={blog.id}
+          user={blog.user}
+          likes={blog.likes}
+          author={blog.author}
+          title={blog.title}
+          url={blog.url}
+        />
+      )
+
+    this.setState({ blogs })
+
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      this.setState({ user })
+      blogService.setToken(user.token)
+    }
+  }
+
+  handleFieldChange = (event) => this.setState({ [event.target.name]: event.target.value })
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      this.setState({ username: '', password: '', user: user })
+      this.notify(`${this.state.user.username} logged in succesfully!`, 'green')
+    } catch (exception) {
+      console.log(exception)
+      this.notify('invalid username or password!', 'red')
+    }
+  }
+
+  logout = () => () => {
+    blogService.setToken(null)
+    window.localStorage.removeItem('loggedInUser')
+    this.notify(`${this.state.user.username} logged out succesfully!`, 'green')
+    this.setState({ user: null })
+  }
+
+  addBlog = () => () => {
+    const newBlog = {
+      title: this.state.newTitle,
+      author: this.state.newAuthor,
+      url: this.state.newUrl
+    }
+    blogService.create(newBlog)
+  }
+
+  notify = (message, color) => {
+    this.setState({ message, color })
+    setTimeout(() => {
+      this.setState({
+        message: null,
+        color: null
+      })
+    }, 2000)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>BLOG LIST</h1>
+        <Notification message={this.state.message} color={this.state.color} />
+        {this.state.user === null ?
+          <div>
+            <Togglable buttonLabel="login">
+              <LoginForm
+                onSubmit={this.login}
+                handleChange={this.handleFieldChange}
+                username={this.state.username}
+                password={this.state.password} />
+            </Togglable>
+          </div> :
+          <div>
+            <h2>{this.state.user.username} logged in <button onClick={this.logout()}>logout</button></h2>
+            <Togglable buttonLabel="new blog">
+              <BlogForm
+                onSubmit={this.addBlog()}
+                handleChange={this.handleFieldChange}
+                title={this.state.newTitle}
+                author={this.state.newAuthor}
+                url={this.state.newUrl} />
+            </Togglable>
+            <h2>Blogs</h2>
+            {this.state.blogs}
+          </div>}
+      </div>
+    )
+  }
+}
+
+export default App
+*/
