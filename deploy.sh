f046ddea483e720971a036ccb362d5419a13dab9
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-02-05 06:58:04 +0200

    favicon and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+npm run build
+rm -rf ../travel-tracker-backend/build
+cp -r build ../travel-tracker-backend/
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import GoogleApiWrapper from './GoogleApiWrapper'
-import { Segment, Embed, List, Header, Card, Menu, Dropdown, Grid, Container, Image, Flag, Icon, Item } from 'semantic-ui-react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Container, Divider } from 'semantic-ui-react'
+import dataService from './services/Data'
+import AddMenu from './components/AddMenu'
+import AddRouting from './components/AddRouting'
 import LoginForm from './LoginForm'
-//import { BrowserRouter as Router, Route, Redirect, NavLink } from 'react-router-dom'
 
 export class App extends Component {
   state = {
@@ -10,12 +13,49 @@ export class App extends Component {
   }
 
   render() {
-    return (
-      this.state.loggedIn ?
-          <GoogleApiWrapper apiKey='AIzaSyCy6G0q6EnGtGPGAAvLlC37STQU4Med0xE' language={'en'} />
-        : <LoginForm />
+    return this.state.loggedIn ? (
+      <GoogleApiWrapper
+        apiKey='AIzaSyCy6G0q6EnGtGPGAAvLlC37STQU4Med0xE'
+        language={'en'}
+      />
+    ) : (
+      <LoginForm />
     )
   }
 }
+
+/*class App extends Component {
+  state = {
+    english: false,
+    data: {
+      en: { bio: [], skills: [], experience: [] },
+      fi: { bio: [], skills: [], experience: [] }
+    }
+  }
+
+  async componentWillMount() {
+    const data = await dataService.getData()
+    this.setState({ data })
+  }
+
+  changeLanguage = () => this.setState({ english: !this.state.english })
+
+  render() {
+    return (
+      <Router>
+        <Container>
+          <Divider hidden />
+          <AddMenu en={this.state.english} change={this.changeLanguage} />
+          <Divider hidden />
+          <Divider hidden />
+          <AddRouting en={this.state.english} data={this.state.data} />
+          <Divider hidden />
+          <Divider hidden />
+          <Divider hidden />
+        </Container>
+      </Router>
+    )
+  }
+}*/
 
 export default App
