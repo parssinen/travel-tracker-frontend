34ac701f9f1052e900e6681e3d8ecd44d49b283d
Lauri Nuotio <lauri.nuotio@siili.com>
2019-08-22 08:38:02 +0300

    removes unnecessary import

--- src/components/GoogleMapsLogin.js
+++ src/components/GoogleMapsLogin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
+import { Map, GoogleApiWrapper } from 'google-maps-react'
 import loginService from '../services/login'
 import travelService from '../services/markers'
 import InfoWindow from './InfoWindow'
