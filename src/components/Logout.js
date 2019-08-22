5f95f0da02e87af61a42e33589419e7a4783ee0d
Lauri Nuotio <lauri.nuotio@siili.com>
2019-08-22 08:34:37 +0300

    translates logout to finnish

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -4,11 +4,11 @@ import { Menu, Icon, Button } from 'semantic-ui-react'
 const Logout = ({ logout }) => {
   return (
     <div>
-      <Menu widths={3} borderless size='huge'>
+      <Menu widths={3} borderless size="huge">
         <Menu.Item>
-          <Button color='blue' onClick={logout} size='large'>
-            <Icon name='log out' />
-            Log out
+          <Button color="blue" onClick={logout} size="large">
+            <Icon name="log out" />
+            Kirjaudu ulos
           </Button>
         </Menu.Item>
       </Menu>
